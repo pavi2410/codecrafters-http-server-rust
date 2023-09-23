d9b844c0ad4642bd8529c4ba1b162e909b1181d8
Pavitra Golchha <hello@pavi2410.me>
2023-09-24 01:20:42 +0530

    push

--- src/main.rs
+++ src/main.rs
@@ -43,6 +43,8 @@ fn handle_stream(mut stream: TcpStream) {
         .filter_map(|l| l.split_once(": "))
         .collect::<HashMap<&str, &str>>();
 
+    let body = lines.skip(1).collect::<String>();
+
     if path == "/" {
         ok_response(stream);
     } else if path.starts_with("/echo") {
@@ -50,15 +52,31 @@ fn handle_stream(mut stream: TcpStream) {
     } else if path == "/user-agent" {
         text_response(stream, headers.get("User-Agent").unwrap());
     } else if path.starts_with("/files") {
-        let file_name = path.strip_prefix("/files/").unwrap();
+        match method {
+            "GET" => {
+                let file_name = path.strip_prefix("/files/").unwrap();
 
-        let directory = args().nth(2).unwrap();
+                let directory = args().nth(2).unwrap();
 
-        let file_path = format!("{}/{}", directory, file_name);
+                let file_path = format!("{}/{}", directory, file_name);
 
-        match std::fs::read(file_path) {
-            Ok(file_contents) => octet_stream_response(stream, &file_contents),
-            Err(_) => not_found_response(stream),
+                match std::fs::read(file_path) {
+                    Ok(file_contents) => octet_stream_response(stream, &file_contents),
+                    Err(_) => not_found_response(stream),
+                }
+            }
+            "POST" => {
+                let file_name = path.strip_prefix("/files/").unwrap();
+
+                let directory = args().nth(2).unwrap();
+
+                let file_path = format!("{}/{}", directory, file_name);
+
+                std::fs::write(file_path, body).unwrap();
+                
+                stream.write(b"HTTP/1.1 201 Created\r\n\r\n").unwrap();
+            }
+            _ => not_found_response(stream),
         }
     } else {
         not_found_response(stream);
@@ -82,7 +100,9 @@ fn text_response(mut stream: TcpStream, text: &str) {
 
 fn octet_stream_response(mut stream: TcpStream, bytes: &[u8]) {
     stream.write(b"HTTP/1.1 200 OK\r\n").unwrap();
-    stream.write(b"Content-Type: application/octet-stream\r\n").unwrap();
+    stream
+        .write(b"Content-Type: application/octet-stream\r\n")
+        .unwrap();
     write!(stream, "Content-Length: {}\r\n\r\n", bytes.len()).unwrap();
     stream.write(bytes).unwrap();
 }
